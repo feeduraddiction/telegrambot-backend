d01fc37a8942efdf4110c23a20ee74932e03a847
Ilya Charniatsou <alfanex62@gmail.com>
2024-05-27 11:37:09 +0400

    test

--- index.js
+++ index.js
@@ -59,6 +59,8 @@ bot.on("message", async (msg) => {
   }
 });
 
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
 app.post("/web-data", async (req, res) => {
   const { queryId, products, totalPrice } = req.body();
   try {
