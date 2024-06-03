d83fbefc8313f9576814ea9f7fb64b0c1e094b42
Ilya Charniatsou <alfanex62@gmail.com>
2024-06-03 14:21:59 +0400

    enable

--- index.js
+++ index.js
@@ -2,114 +2,115 @@ const TelegramBot = require("node-telegram-bot-api");
 const express = require("express");
 const cors = require("cors");
 
-// const token = "7061584216:AAHFHLH9kGT33IxRlkaSKWVeoQtaCkZtn4M";
+const token = "7061584216:AAHFHLH9kGT33IxRlkaSKWVeoQtaCkZtn4M";
 
-// const webAppUrl = "https://192.168.1.155:3000";
+const webAppUrl = "https://master--telegrambot322.netlify.app";
 
-// const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(token, { polling: true });
 
-// const app = express();
+const app = express();
 
-// app.use(express.json());
+app.use(express.json());
 
-// app.use(
-//   cors({
-//     origin: [
-//       "https://192.168.1.155:3000",
-//       "https://master--telegrambot322.netlify.app",
-//     ],
-//   })
-// );
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://master--telegrambot322.netlify.app",
+    ],
+  })
+);
 
-// bot.on("message", async (msg) => {
-//   const chatId = msg.chat.id;
-//   const text = msg.text;
+bot.on("message", async (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
 
-//   console.log(msg);
+  console.log(msg);
 
-//   if (text === "/huesos") {
-//     bot.sendMessage(chatId, "( . )( . )");
-//   }
+  if (text === "/huesos") {
+    bot.sendMessage(chatId, "( . )( . )");
+  }
 
-//   if (text === "/start") {
-//     await bot.sendMessage(chatId, "По нажатию кнопки заполните форму", {
-//       reply_markup: {
-//         keyboard: [
-//           [{ text: "Заполнить форму", web_app: { url: webAppUrl + "/form" } }],
-//         ],
-//       },
-//     });
-//   }
+  if (text === "/start") {
+    await bot.sendMessage(chatId, "По нажатию кнопки заполните форму", {
+      reply_markup: {
+        keyboard: [
+          [{ text: "Заполнить форму", web_app: { url: webAppUrl + "/form" } }],
+        ],
+      },
+    });
+  }
 
-//   if (text === "/start") {
-//     await bot.sendMessage(chatId, "Заходите в наш магазин", {
-//       reply_markup: {
-//         inline_keyboard: [
-//           [{ text: "Сделать заказ", web_app: { url: webAppUrl } }],
-//         ],
-//       },
-//     });
-//   }
+  if (text === "/start") {
+    await bot.sendMessage(chatId, "Заходите в наш магазин", {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: "Сделать заказ", web_app: { url: webAppUrl } }],
+        ],
+      },
+    });
+  }
 
-//   if (msg.web_app_data?.data) {
-//     try {
-//       const data = JSON.parse(msg?.web_app_data?.data);
-//       console.log("be", data);
-//       await bot.sendMessage(chatId, "Спасибо за информацию");
-//       await bot.sendMessage(chatId, "Ваша страна: " + data?.country);
-//       await bot.sendMessage(chatId, "Ваша город: " + data?.city);
-//       setTimeout(async () => {
-//         await bot.sendMessage(chatId, "Всю информацию вы получите в этом чате");
-//       }, 1500);
-//     } catch (e) {
-//       console.log(e);
-//     }
-//   }
-// });
+  if (msg.web_app_data?.data) {
+    try {
+      const data = JSON.parse(msg?.web_app_data?.data);
+      console.log("be", data);
+      await bot.sendMessage(chatId, "Спасибо за информацию");
+      await bot.sendMessage(chatId, "Ваша страна: " + data?.country);
+      await bot.sendMessage(chatId, "Ваша город: " + data?.city);
+      setTimeout(async () => {
+        await bot.sendMessage(chatId, "Всю информацию вы получите в этом чате");
+      }, 1500);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+});
 
-// app.get("/", (req, res) => {
-//   console.log("Root route accessed");
-//   res.send("Express on Vercel");
-// });
+app.get("/", (req, res) => {
+  console.log("Root route accessed");
+  res.send("Express on Vercel");
+});
 
-// app.get("/check-connection", (req, res) => {
-//   res.send({ data: "OK" });
-// });
+app.get("/check-connection", (req, res) => {
+  res.send({ data: "OK" });
+  console.log('ok')
+});
 
-// app.post("/web-data", async (req, res) => {
-//   try {
-//     const { queryId, products, totalPrice } = req.body;
+app.post("/web-data", async (req, res) => {
+  try {
+    const { queryId, products, totalPrice } = req.body;
 
-//     const query = await bot.answerWebAppQuery(queryId, {
-//       type: "article",
-//       id: queryId,
-//       title: "Успешная покупка",
-//       input_message_content: {
-//         message_text:
-//           "Поздравляю с покупкой, приобретен товар на сумму " + totalPrice,
-//       },
-//     });
+    const query = await bot.answerWebAppQuery(queryId, {
+      type: "article",
+      id: queryId,
+      title: "Успешная покупка",
+      input_message_content: {
+        message_text:
+          "Поздравляю с покупкой, приобретен товар на сумму " + totalPrice,
+      },
+    });
 
-//     bot.sendInvoice(queryId, "title", "description", "payload");
+    bot.sendInvoice(queryId, "title", "description", "payload");
 
-//     console.log(query);
-//     await bot.sendInvoice(queryId, "hello");
-//     return res.status(200).json({ status: "OK" });
-//   } catch (e) {
-//     const { queryId } = req.body;
+    console.log(query);
+    await bot.sendInvoice(queryId, "hello");
+    return res.status(200).json({ status: "OK" });
+  } catch (e) {
+    const { queryId } = req.body;
 
-//     await bot.answerWebAppQuery(queryId, {
-//       type: "article",
-//       id: queryId,
-//       title: "Не удалось совершить покупку",
-//       input_message_content: {
-//         message_text: "Не удалось приобрести товар",
-//       },
-//     });
-//     return res.status(500);
-//   }
-// });
+    await bot.answerWebAppQuery(queryId, {
+      type: "article",
+      id: queryId,
+      title: "Не удалось совершить покупку",
+      input_message_content: {
+        message_text: "Не удалось приобрести товар",
+      },
+    });
+    return res.status(500);
+  }
+});
 
-// const PORT = 8000;
+const PORT = 8000;
 
-// app.listen(PORT, () => console.log("server started at port " + 8000));
+app.listen(PORT, () => console.log("server started at port " + 8000));
