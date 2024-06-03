d22ab168768e97666435dc316d7457c412496cce
Ilya Charniatsou <alfanex62@gmail.com>
2024-06-03 16:00:10 +0400

    error

--- index.js
+++ index.js
@@ -101,17 +101,18 @@ app.post("/web-data", async (req, res) => {
 
     return res.status(200).json({ status: "OK" });
   } catch (e) {
-    const { queryId } = req.body;
+    console.log(e)
+    // const { queryId } = req.body;
 
-    await bot.answerWebAppQuery(queryId, {
-      type: "article",
-      id: queryId,
-      title: "Не удалось совершить покупку",
-      input_message_content: {
-        message_text: "Не удалось приобрести товар",
-      },
-    });
-    return res.status(500);
+    // await bot.answerWebAppQuery(queryId, {
+    //   type: "article",
+    //   id: queryId,
+    //   title: "Не удалось совершить покупку",
+    //   input_message_content: {
+    //     message_text: "Не удалось приобрести товар",
+    //   },
+    // });
+    // return res.status(500);
   }
 });
 
